df3ff72e13ed42e650f82c2a3dbe7d1867758747
Mauro <mauro.cioni@gmail.com>
2021-07-26 20:46:55 +0200

    prova

--- src/app/dir-all.directive.ts
+++ src/app/dir-all.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef } from "@angular/core";
+import { Directive, ElementRef, Self } from "@angular/core";
 
 @Directive({
   selector: "div.card",
 })
 export class DirAllDirective {
-  constructor(el: ElementRef) {
+  constructor(el: ElementRef, @Self() private readonly self: ElementRef) {
     el.nativeElement.style.backgroundColor = "yellow";
+    this.self.nativeElement.style.backgroundColor = "magenta";
+    console.log(this.self);
   }
 }
